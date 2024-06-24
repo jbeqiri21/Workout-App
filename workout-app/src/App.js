6a3ff326746a7ce21d91740e63fb5771ca51d1ae
Harsh Patel <hpatel1@iuw.edu>
2024-06-23 19:36:43 -0500

    Created the landing page

--- workout-app/src/App.js
+++ workout-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import About from './components/About';
+import Login from './components/Login';
+import Register from './components/Register';
+import AIWorkout from './components/AIWorkout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/ai-workout" element={<AIWorkout />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
